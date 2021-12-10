c4a32a5d405ea3e6d8bcb58b89adf897fd37fe56
Kaiden Wealch <kwealch@lcs.on.ca>
2021-12-10 14:30:01 -0500

    Got diamonds beside cross.

--- Playground.playground/Pages/Empty Canvas 13.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 13.xcplaygroundpage/Contents.swift
@@ -1,10 +1,10 @@
 //: [Previous](@previous) / [Next](@next)
 /*:
-## Canvas size
+ ## Canvas size
  
  Set the size of your desired canvas by adjusting the constants on lines 7 and 8.
  */
-let preferredWidth = 600
+let preferredWidth = 400
 let preferredHeight = 600
 /*:
  ## Required code
@@ -41,37 +41,245 @@ PlaygroundPage.current.liveView = canvas
  */
 
 // Move the origin from the bottom-left corner of the canvas to it's centre point
-canvas.translate(to: Point(x: canvas.width / 2,
-                           y: canvas.height / 2))
+//canvas.translate(to: Point(x: canvas.width / 2,
+//y: canvas.height / 2))
 
 // Show a grid
-canvas.drawAxes(withScale: true, by: 20, color: .black)
+canvas.drawAxes(withScale: true, by: 50, color: .black)
 
 /*:
  ## Add your code
  
  Beginning on line 61, you can add your own code.
-  
+ 
  [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
-
+ 
  */
-
 // Begin writing your code below (you can remove the examples shown)
+//Background Rectangle
+let currentColor = Color(hue: 323,
+                         saturation: 93,
+                         brightness: 70,
+                         alpha: 100)
+canvas.fillColor = currentColor
 
-// Draw a circle, using the canvas object directly
-canvas.drawEllipse(at: Point(x: 100, y: 100), width: 25, height: 25)
+canvas.drawRectangle(at: Point(x: 0, y: 0),
+                     width: 400,
+                     height: 600)
 
-// Draw a vertical line, up and to the left
-p.drawTo(dx: -25, dy: 50)
 
-// Go back to origin
-p.goToOrigin()
+//Vertical Rectangles
 
-// Change the pen color
-p.penColor = .red
+for xPosition in stride(from: 0,
+                        through: 400,
+                        by: 25){
+    
+    
+    if xPosition == 0{
+    let currentColor = Color(hue: 235,
+                             saturation: 57,
+                             brightness: 80,
+                             alpha: 100)
+        canvas.fillColor = currentColor
+        
+    }else if xPosition == 25{
+        let currentColor = Color(hue: 0,
+                                 saturation: 66,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 50{
+        let currentColor = Color(hue: 120,
+                                 saturation: 67,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 75{
+        let currentColor = Color(hue: 235,
+                                 saturation: 57,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 100{
+        let currentColor = Color(hue: 0,
+                                 saturation: 66,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 125{
+        let currentColor = Color(hue: 120,
+                                 saturation: 67,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 150{
+        let currentColor = Color(hue: 235,
+                                 saturation: 57,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 175{
+        let currentColor = Color(hue: 0,
+                                 saturation: 66,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 200{
+        let currentColor = Color(hue: 120,
+                                 saturation: 67,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 225{
+        let currentColor = Color(hue: 235,
+                                 saturation: 57,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 250{
+        let currentColor = Color(hue: 0,
+                                 saturation: 66,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 275{
+        let currentColor = Color(hue: 120,
+                                 saturation: 67,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 300{
+        let currentColor = Color(hue: 235,
+                                 saturation: 57,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 325{
+        let currentColor = Color(hue: 0,
+                                 saturation: 66,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 350{
+        let currentColor = Color(hue: 120,
+                                 saturation: 67,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 375{
+        let currentColor = Color(hue: 235,
+                                 saturation: 57,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }else if xPosition == 400{
+        let currentColor = Color(hue: 0,
+                                 saturation: 66,
+                                 brightness: 80,
+                                 alpha: 100)
+            canvas.fillColor = currentColor
+        
+    }
+    
+    
+    canvas.drawRectangle(at: Point(x: xPosition, y: 0),
+                         width: 23,
+                         height: 400)
+}
 
-// Draw a curve, down and to the right
-p.addArc(radius: 50, angle: -45)
+//Horrizontal Rectangle
+
+let orange = Color(hue: 27,
+                         saturation: 66,
+                         brightness: 90,
+                         alpha: 100)
+canvas.fillColor = orange
+
+canvas.drawRectangle(at: Point(x: 0, y: 100), width: 400, height: 225)
+
+canvas.fillColor = .blue
+
+//Cross
+canvas.drawRectangle(at: Point(x: 125, y: 200),
+                     width: 150,
+                     height: 50)
+
+canvas.drawRectangle(at: Point(x: 175, y: 125),
+                     width: 50,
+                     height: 175)
+
+//Diamonds Beside Cross #1
+var diamondVerticies: [Point] = []
+diamondVerticies.append(Point(x: 350, y: 275)) //A
+diamondVerticies.append(Point(x: 400, y: 225)) //B
+diamondVerticies.append(Point(x: 350, y: 175)) //C
+diamondVerticies.append(Point(x: 300, y: 225)) //D
+canvas.drawCustomShape(with: diamondVerticies)
+
+//Diamonds Beside Cross #2
+var squareVerticies: [Point] = []
+squareVerticies.append(Point(x: 50, y: 275)) //A
+squareVerticies.append(Point(x: 100, y: 225)) //B
+squareVerticies.append(Point(x: 50, y: 175)) //C
+squareVerticies.append(Point(x: 0, y: 225)) //D
+canvas.drawCustomShape(with: squareVerticies)
+
+//Title
+canvas.textColor = .white
+canvas.drawText(message: "kanye west",
+                at: Point(x: 12,
+                          y: 409),
+                size: 55)
+
+//Details Group 1
+
+canvas.drawText(message: "october 15, 2016",
+                at: Point(x: 50,
+                          y: 550),
+                size: 10)
+
+//Group 2
+
+canvas.drawText(message: "in",
+                at: Point(x: 175,
+                          y: 550),
+                size: 10)
+
+canvas.drawText(message: "rogers centre",
+                at: Point(x: 175,
+                          y: 538),
+                size: 10)
+
+//Group 3
+
+canvas.drawText(message: "edmonton, alberta",
+                at: Point(x: 300,
+                          y: 550),
+                size: 10)
+
+canvas.drawText(message: "canada",
+                at: Point(x: 300,
+                          y: 538),
+                size: 10)
+
+
+
+
 
 /*:
  ## Show the Live View
@@ -80,7 +288,7 @@ p.addArc(radius: 50, angle: -45)
  Remember to show the Live View (1 then 2):
  
  ![timeline](timeline.png "Timeline")
-
+ 
  ## Use source control
  To keep your work organized, receive feedback, and earn a high grade in this course, regular use of source control is a must.
  
